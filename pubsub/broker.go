acf11c135fbbd0fc1e1b0f92eacd0bebf5b75f8d
felix <wangfelix87@gmail.com>
2018-04-04 22:06:51 +0800

    format codes

--- pubsub/broker.go
+++ pubsub/broker.go
@@ -1,113 +1,113 @@
 package pubsub
 
 import (
-    "crypto/rand"
-    "encoding/hex"
-    "sync"
-    "time"
+	"crypto/rand"
+	"encoding/hex"
+	"sync"
+	"time"
 )
 
+// Pubsub Broker
 type Broker struct {
-    subscribers Subscribers
-    slock       sync.RWMutex
-    topics      map[string]Subscribers
-    tlock       sync.RWMutex
+	subscribers Subscribers
+	slock       sync.RWMutex
+	topics      map[string]Subscribers
+	tlock       sync.RWMutex
 }
 
 // create new broker
 func NewBroker() *Broker {
-    return &Broker{
-        // subscribers: Subscribers{},
-        slock:       sync.RWMutex{},
-        topics:      map[string]Subscribers{},
-        tlock:       sync.RWMutex{},
-    }
+	return &Broker{
+		// subscribers: Subscribers{},
+		slock:  sync.RWMutex{},
+		topics: map[string]Subscribers{},
+		tlock:  sync.RWMutex{},
+	}
 }
 
 // create a new subscriber and register it into the broker
 func (b *Broker) Attach() (*Subscriber, error) {
-    // b.slock.Lock()
-    // defer b.slock.Unlock()
-    id := make([]byte, 50)
-    if _, err := rand.Read(id); err != nil {
-        return nil, err
-    }
+	// b.slock.Lock()
+	// defer b.slock.Unlock()
+	id := make([]byte, 50)
+	if _, err := rand.Read(id); err != nil {
+		return nil, err
+	}
 
-    s := &Subscriber{
-        id:        hex.EncodeToString(id),
-        messages:  make(chan *Message),
-        createAt:  time.Now().UnixNano(),
-        destroyed: false,
-        lock:      &sync.RWMutex{},
-        topics:    map[string]bool{},
-    }
-    // b.subscribers[s.id] = s
-    return s, nil
+	s := &Subscriber{
+		id:        hex.EncodeToString(id),
+		messages:  make(chan *Message),
+		createAt:  time.Now().UnixNano(),
+		destroyed: false,
+		lock:      &sync.RWMutex{},
+		topics:    map[string]bool{},
+	}
+	// b.subscribers[s.id] = s
+	return s, nil
 }
 
 // remove the specific subscriber from the broker
 func (b *Broker) Detach(s *Subscriber) {
-    // b.slock.Lock()
-    // defer b.slock.Unlock()
-    s.Destroy()
-    b.Unsubscribe(s, s.GetTopics()...)
+	// b.slock.Lock()
+	// defer b.slock.Unlock()
+	s.Destroy()
+	b.Unsubscribe(s, s.GetTopics()...)
 }
 
 // subscribes the specific subscriber "s" to the specific list of topic(s)
 func (b *Broker) Subscribe(s *Subscriber, topics ...string) {
-    b.tlock.Lock()
-    defer b.tlock.Unlock()
-    for _, topic := range topics {
-        if nil == b.topics[topic] {
-            b.topics[topic] = Subscribers{}
-        }
-        s.topics[topic] = true
-        b.topics[topic][s.id] = s
-    }
+	b.tlock.Lock()
+	defer b.tlock.Unlock()
+	for _, topic := range topics {
+		if nil == b.topics[topic] {
+			b.topics[topic] = Subscribers{}
+		}
+		s.topics[topic] = true
+		b.topics[topic][s.id] = s
+	}
 }
 
 // unsubscribes the specific subscriber from the specific topic(s)
 func (b *Broker) Unsubscribe(s *Subscriber, topics ...string) {
-    b.tlock.Lock()
-    defer b.tlock.Unlock()
-    for _, topic := range topics {
-        if nil == b.topics[topic] {
-            continue
-        }
-        delete(b.topics[topic], s.id)
-        delete(s.topics, topic)
-    }
+	b.tlock.Lock()
+	defer b.tlock.Unlock()
+	for _, topic := range topics {
+		if nil == b.topics[topic] {
+			continue
+		}
+		delete(b.topics[topic], s.id)
+		delete(s.topics, topic)
+	}
 }
 
 // broadcast the specific payload to all the topic(s) subscribers
 func (b *Broker) Broadcast(data []byte, topics ...string) {
-    // b.tlock.RLock()
-    // defer b.tlock.RUnlock()
-    for _, topic := range topics {
-        if nil == b.topics[topic] {
-            continue
-        }
-        for _, s := range b.topics[topic] {
-            m := &Message{
-                topic:      topic,
-                data:       data,
-                createAt:   time.Now().UnixNano(),
-            }
+	// b.tlock.RLock()
+	// defer b.tlock.RUnlock()
+	for _, topic := range topics {
+		if nil == b.topics[topic] {
+			continue
+		}
+		for _, s := range b.topics[topic] {
+			m := &Message{
+				topic:    topic,
+				data:     data,
+				createAt: time.Now().UnixNano(),
+			}
 
-            
-            go (func(s *Subscriber) {
-                s.Signal(m)
-            })(s)
+			go (func(s *Subscriber) {
+				s.Signal(m)
+			})(s)
 
-            // s.Signal(m)
-        }
-    }
+			// s.Signal(m)
+		}
+	}
 }
 
 // get the subscribers count
 func (b *Broker) Subscribers(topic string) int {
-    b.tlock.RLock()
-    defer b.tlock.RUnlock()
-    return len(b.topics[topic])
-    return 0
+	b.tlock.RLock()
+	defer b.tlock.RUnlock()
+	return len(b.topics[topic])
+	return 0
 }
--- ws-relay.go
+++ ws-relay.go
@@ -1,182 +1,176 @@
 package main
 
 import (
-    "context"
-    "flag"
-    "net/http"
-    "net/http/pprof"
-    "os"
-    "os/signal"
-    "time"
-    "io"
+	"context"
+	"flag"
+	"io"
+	"net/http"
+	"net/http/pprof"
+	"os"
+	"os/signal"
+	"time"
 
-    "github.com/gorilla/mux"
-    "github.com/numb3r3/jsmpeg-relay/pubsub"
-    "github.com/numb3r3/jsmpeg-relay/log"
-    "github.com/numb3r3/jsmpeg-relay/websocket"
+	"github.com/gorilla/mux"
+	"github.com/numb3r3/jsmpeg-relay/log"
+	"github.com/numb3r3/jsmpeg-relay/pubsub"
+	"github.com/numb3r3/jsmpeg-relay/websocket"
 )
 
 //Broker default
 var broker = pubsub.NewBroker()
 
-
 func publishHandler(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    app_name := vars["app_name"]
-    stream_key := vars["stream_key"]
-    // logging.Infof("publish stream %v / %v", app_name, stream_key)
-    if r.Body != nil {
-        logging.Debugf("publishing stream %v / %v from %v", app_name, stream_key, r.RemoteAddr)
-        
-        buf := make([]byte, 1024*1024)
-        for {
-            n, err := r.Body.Read(buf)
-            if err == io.EOF {
-                break
-            } else if err != nil {
-                logging.Error("[stream][recv] error:", err)
-                return
-            }
-            if n > 0 {
-                // logging.Info("broadcast stream")
-                broker.Broadcast(buf[:n], app_name + "/" + stream_key)
-            }
-        }
-    }
-    defer r.Body.Close()
-    w.WriteHeader(200)
-    flusher := w.(http.Flusher)
-    flusher.Flush()
-    
+	vars := mux.Vars(r)
+	appName := vars["app_name"]
+	streamKey := vars["stream_key"]
+
+	// logging.Infof("publish stream %v / %v", app_name, stream_key)
+	if r.Body != nil {
+		logging.Debugf("publishing stream %v / %v from %v", appName, streamKey, r.RemoteAddr)
+
+		buf := make([]byte, 1024*1024)
+		for {
+			n, err := r.Body.Read(buf)
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				logging.Error("[stream][recv] error:", err)
+				return
+			}
+			if n > 0 {
+				// logging.Info("broadcast stream")
+				broker.Broadcast(buf[:n], appName+"/"+streamKey)
+			}
+		}
+	}
+	defer r.Body.Close()
+	w.WriteHeader(200)
+	flusher := w.(http.Flusher)
+	flusher.Flush()
+
 }
 
+func playHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	appName := vars["app_name"]
+	streamKey := vars["stream_key"]
 
-func playHandler(w http.ResponseWriter, r *http.Request){
-    vars := mux.Vars(r)
-    app_name := vars["app_name"]
-    stream_key := vars["stream_key"]
-    logging.Infof("play stream %v / %v", app_name, stream_key)
+	logging.Infof("play stream %v / %v", appName, streamKey)
 
-    c, ok := websocket.TryUpgrade(w, r)
-    if ok != true {
-        logging.Error("[ws] upgrade failed")
-        return 
-    }
-    // cleanup on server side
-    // defer c.Close()
+	c, ok := websocket.TryUpgrade(w, r)
+	if ok != true {
+		logging.Error("[ws] upgrade failed")
+		return
+	}
+	// cleanup on server side
+	// defer c.Close()
 
+	subscriber, err := broker.Attach()
+	if err != nil {
+		c.Close()
+		logging.Error("subscribe error: ", err)
+		return
+	}
+	// defer broker.Detach(subscriber)
+	defer func() {
+		logging.Debug("websocket closed: to unsubscribe")
+		broker.Detach(subscriber)
+		c.Close()
+	}()
 
-    subscriber, err := broker.Attach()
-    if err != nil {
-        c.Close()
-        logging.Error("subscribe error: ", err)
-        return
-    }
-    // defer broker.Detach(subscriber)
-    defer func() {
-        logging.Debug("websocket closed: to unsubscribe")
-        broker.Detach(subscriber)
-        c.Close()
-    }()
-    
+	// go func() {
 
-    // go func() {
+	//     select {
+	//     case _, ok := <-c.Closing():
+	//         if ok {
+	//             logging.Debug("websocket closed: to unsubscribe")
+	//             broker.Detach(subscriber)
+	//         } else {
+	//             logging.Debug("closing channel is closed")
+	//         }
+	//         return
+	//     default:
+	//     }
 
-    //     select {
-    //     case _, ok := <-c.Closing():
-    //         if ok {
-    //             logging.Debug("websocket closed: to unsubscribe")
-    //             broker.Detach(subscriber)
-    //         } else {
-    //             logging.Debug("closing channel is closed")
-    //         }
-    //         return
-    //     default:
-    //     }
+	// }()
 
-    // }()
-   
+	logging.Info("client remote addr: ", c.RemoteAddr())
 
-    logging.Info("client remote addr: ", c.RemoteAddr())
+	broker.Subscribe(subscriber, appName+"/"+streamKey)
+	for {
+		select {
+		// case <- c.Closing():
+		//     logging.Info("websocket closed: to unsubscribe")
+		//     broker.Detach(subscriber)
+		case msg := <-subscriber.GetMessages():
+			// logging.Info("[stream][send]")
+			data := msg.GetData()
+			_, err := c.Write(data)
+			if err != nil {
+				// c.Close()
+				logging.Debug("to unsubscribe")
+				broker.Detach(subscriber)
+				logging.Error("write mesage error: ", err)
+				return
+			}
 
-    
-    broker.Subscribe(subscriber, app_name + "/" + stream_key)
-    for  {
-        select {
-        // case <- c.Closing():
-        //     logging.Info("websocket closed: to unsubscribe")
-        //     broker.Detach(subscriber)
-        case msg := <-subscriber.GetMessages():
-            // logging.Info("[stream][send]")
-            data := msg.GetData()
-            _, err := c.Write(data)
-            if err != nil {
-                // c.Close()
-                logging.Debug("to unsubscribe")
-                broker.Detach(subscriber)
-                logging.Error("write mesage error: ", err)
-                return
-            }
-
-        }
-    }
-    return
+		}
+	}
+	return
 }
-
-
 
 func main() {
-    var listen_addr = flag.String("l", "0.0.0.0:8080", "")
-    var wait time.Duration
-    flag.DurationVar(&wait, "graceful-timeout", time.Second * 15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
-    flag.Parse()
+	var listenAddr = flag.String("l", "0.0.0.0:8080", "")
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
 
-    logging.Info("start ws-relay ....")
-    logging.Infof("server listen @ %v", *listen_addr)
+	logging.Info("start ws-relay ....")
+	logging.Infof("server listen @ %v", *listenAddr)
 
-    r := mux.NewRouter()
-    r.HandleFunc("/publish/{app_name}/{stream_key}", publishHandler).Methods("POST")
-    r.HandleFunc("/play/{app_name}/{stream_key}", playHandler)
-    r.HandleFunc("/debug/pprof/", pprof.Index)
-    r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-    r.HandleFunc("/debug/pprof/profile", pprof.Profile)
-    r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-    r.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	r := mux.NewRouter()
+	r.HandleFunc("/publish/{app_name}/{stream_key}", publishHandler).Methods("POST")
+	r.HandleFunc("/play/{app_name}/{stream_key}", playHandler)
+	r.HandleFunc("/debug/pprof/", pprof.Index)
+	r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	r.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	r.HandleFunc("/debug/pprof/trace", pprof.Trace)
 
-    srv := &http.Server{
-        Addr:         *listen_addr,
-        // Good practice to set timeouts to avoid Slowloris attacks.
-        WriteTimeout: time.Second * 0,
-        ReadTimeout:  time.Second * 0,
-        IdleTimeout:  time.Second * 60,
-        Handler: r, // Pass our instance of gorilla/mux in.
-    }
+	srv := &http.Server{
+		Addr: *listenAddr,
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 0,
+		ReadTimeout:  time.Second * 0,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r, // Pass our instance of gorilla/mux in.
+	}
 
-    // Run our server in a goroutine so that it doesn't block.
-    go func() {
-        if err := srv.ListenAndServe(); err != nil {
-            logging.Error("server listen error:", err)
-        }
-    }()
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			logging.Error("server listen error:", err)
+		}
+	}()
 
-    c := make(chan os.Signal, 1)
-    // We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
-    // SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
-    signal.Notify(c, os.Interrupt)
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
 
-    // Block until we receive our signal.
-    <-c
+	// Block until we receive our signal.
+	<-c
 
-    // Create a deadline to wait for.
-    ctx, cancel := context.WithTimeout(context.Background(), wait)
-    defer cancel()
-    // Doesn't block if no connections, but will otherwise wait
-    // until the timeout deadline.
-    srv.Shutdown(ctx)
-    // Optionally, you could run srv.Shutdown in a goroutine and block on
-    // <-ctx.Done() if your application should wait for other services
-    // to finalize based on context cancellation.
-    logging.Info("shutting down")
-    os.Exit(0)
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	logging.Info("shutting down")
+	os.Exit(0)
 
 }
