e2a8a2df28e0df2d587afbcf4807029bbf641fc2
felix <wangfelix87@gmail.com>
2018-04-05 17:22:21 +0800

    close websockt as ping fails

--- websocket/websocket.go
+++ websocket/websocket.go
@@ -84,7 +84,7 @@ func newWebsocketConn(ws websocketConn) *websocketTransport {
 	      // logging.Debug("to write ping")
 	      if err := ws.WriteControl(websocket.PingMessage, []byte{}, time.Now().Add(writeWait)); err != nil {
 	          logging.Debug("ping err: ", err)
-	          // conn.closing <- true
+	          conn.closing <- true
 	      }
 	  }, pingPeriod, conn.closing)
 
